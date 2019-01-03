a51c5d3664d96c7d400f0aa9003f08025177ec2e
crvernon <chrisrvernon@gmail.com>
2019-01-03 16:20:33 -0500

    adding init file with version

---
+++ extractor/__init__.py
@@ -0,0 +1,9 @@
+import pkg_resources
+"""Extractor - functionality for extracting and formatting data from GCAM databases
+
+Common uses are to prepare data from a GCAM database for use by other models and tools.
+"""
+
+__version__ = pkg_resources.get_distribution('extractor').version
+
+__all__ = ['to_demeter']
