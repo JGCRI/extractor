f9afe44cd53c4df4e3b7e32f8509b86ec001e6bc
crvernon <chrisrvernon@gmail.com>
2019-02-06 11:07:31 -0500

    adding package level class import

--- extractor/__init__.py
+++ extractor/__init__.py
@@ -1,9 +1,13 @@
-import pkg_resources
 """Extractor - functionality for extracting and formatting data from GCAM databases
 
 Common uses are to prepare data from a GCAM database for use by other models and tools.
 """
+import pkg_resources
+
+from extractor.to_demeter import GcamToDemeter
+from extractor.demeter_landclass_split import GcamLandclassSplit
+
 
 __version__ = pkg_resources.get_distribution('extractor').version
 
-__all__ = ['to_demeter', 'landclass_redistribute']
+__all__ = ['to_demeter', 'demeter_landclass_split']
